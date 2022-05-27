cdf8f5e3312c9d01f914c698cd3c2cc7b0fbd7a4
Seth Thomas <sthomas@8thlight.com>
2022-05-27 09:19:37 -0400

    [FEATURE] Set up skeleton of elixir project for game of life

---
+++ lib/gol.ex
@@ -0,0 +1,18 @@
+defmodule Gol do
+  @moduledoc """
+  Documentation for `Gol`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gol.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
