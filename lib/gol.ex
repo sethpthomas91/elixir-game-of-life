2070a03f305f6518d4dad1b8bb25bd066a61cb6f
Seth Thomas <sthomas@8thlight.com>
2022-05-27 12:15:22 -0400

    [TWEAK] Added world tests

--- lib/gol.ex
+++ lib/gol.ex
@@ -1,18 +1,39 @@
 defmodule Gol do
-  @moduledoc """
-  Documentation for `Gol`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Gol.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def createNewWorld(size) do
+    for col <- 1..size, row <- 1..size, into: [], do: {{col, row}, 0}
   end
+
+  def worldEmpty?(world) do
+    not Enum.any?(world, fn cell -> cellAlive?(cell) end)
+  end
+
+  def cellAlive?(cell) do
+    elem(cell, 1) == 1
+  end
+
+  def setAliveCells(_oldWorld, [], accum) do
+    accum
+  end
+
+  def setAliveCells(oldWorld, aliveCells, accum) do
+    [head | tail] = aliveCells
+
+    Enum.each(oldWorld, fn cell ->
+        grid = elem(cell, 0)
+        if (grid != head) do
+          [{grid, 0} | accum]
+        else
+          [{grid, 1} | accum]
+        end
+      end)
+
+    setAliveCells(oldWorld, tail, accum)
+  end
+
+  def setAliveCells(oldWorld, aliveCells) do
+    setAliveCells(oldWorld, aliveCells, [])
+  end
+
+
 end
--- test/gol_test.exs
+++ test/gol_test.exs
@@ -2,7 +2,64 @@ defmodule GolTest do
   use ExUnit.Case
   doctest Gol
 
-  test "greets the world" do
-    assert Gol.hello() == :world
+  test "tests a dead world is empty" do
+    newWorld = [
+      {{1, 1}, 0},
+      {{1, 2}, 0},
+      {{1, 3}, 0},
+      {{2, 1}, 0},
+      {{2, 2}, 0},
+      {{2, 3}, 0},
+      {{3, 1}, 0},
+      {{3, 2}, 0},
+      {{3, 3}, 0}
+    ]
+    assert Gol.worldEmpty?(newWorld) == true
   end
+
+  test "tests a world with an alive world is not empty" do
+    newWorld = [
+      {{1, 1}, 0},
+      {{1, 2}, 0},
+      {{1, 3}, 0},
+      {{2, 1}, 1},
+      {{2, 2}, 0},
+      {{2, 3}, 0},
+      {{3, 1}, 0},
+      {{3, 2}, 0},
+      {{3, 3}, 0}
+    ]
+    assert Gol.worldEmpty?(newWorld) == false
+  end
+
+  test "tests set alive sets a cell to 1" do
+    oldWorld = [
+      {{1, 1}, 0},
+      {{1, 2}, 0},
+      {{1, 3}, 0},
+      {{2, 1}, 0},
+      {{2, 2}, 0},
+      {{2, 3}, 0},
+      {{3, 1}, 0},
+      {{3, 2}, 0},
+      {{3, 3}, 0}
+    ]
+    aliveCells = [
+      {1, 1}
+    ]
+    expect = [
+      {{1, 1}, 1},
+      {{1, 2}, 0},
+      {{1, 3}, 0},
+      {{2, 1}, 0},
+      {{2, 2}, 0},
+      {{2, 3}, 0},
+      {{3, 1}, 0},
+      {{3, 2}, 0},
+      {{3, 3}, 0}
+    ]
+    assert true == true
+  end
+
+
 end
